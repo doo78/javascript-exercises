caeb44a5bd2b0a01db88c400f67afc937f13e021
doo78 <rickydeangordon@gmail.com>
2024-08-30 15:35:46 +0100

    Did some exercises, but not al of them were necessary.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(theString, number) {
+    let answer = ""
+    for (let i = 0; i < number; i++){
+        answer += theString;
+    }
 
+    if (number < 0){
+        return "ERROR"
+    }
+    
+    return answer;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    if (str.length == 0){
+        return "";
+    }
 
+    let answer = "";
+    for (let i = 0; i < str.length + 1; i++){
+        answer += str.slice(str.length - i, str.length - i + 1);
+    }
+    
+    return answer;
 };
 
 // Do not edit below this line
